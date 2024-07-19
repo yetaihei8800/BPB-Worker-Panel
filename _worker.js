829a859a539686badfc977a98cbd83bc3cee8da9
yetaihei8800 <62210070+yetaihei8800@users.noreply.github.com>
2024-07-20 02:09:06 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -7,7 +7,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // https://www.uuidgenerator.net/
-let userID = '89b3cbba-e6ac-485a-9481-976a0415eab9';
+let userID = '1c3ff64a-f044-469a-a596-1c7a530cae0d';
 
 // https://www.nslookup.io/domains/cdn.xn--b6gac.eu.org/dns-records/
 // https://www.nslookup.io/domains/cdn-all.xn--b6gac.eu.org/dns-records/
@@ -200,7 +200,7 @@ export default {
 
                     default:
                         // return new Response('Not found', { status: 404 });
-                        url.hostname = 'www.speedtest.net';
+                        url.hostname = 'www.laowu.link';
                         url.protocol = 'https:';
                         request = new Request(url, request);
                         return await fetch(request);
